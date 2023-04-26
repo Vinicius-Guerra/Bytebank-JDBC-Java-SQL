54f39a22c7c382a2483fe769158b81d62034a96e
Vinicius Guerra <102999190+Vinicius-Guerra@users.noreply.github.com>
2023-04-26 18:50:40 -0300

    Conectando com banco
    Conexao com banco e implementando codigo de abrir conta atraves da classe DAO

---
+++ byte-bank-alura-curso_jdbc_inicio/bytebank1/src/main/java/com/mycompany/bytebank1/Bytebank1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bytebank1;
+
+/**
+ *
+ * @author vinif
+ */
+public class Bytebank1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
