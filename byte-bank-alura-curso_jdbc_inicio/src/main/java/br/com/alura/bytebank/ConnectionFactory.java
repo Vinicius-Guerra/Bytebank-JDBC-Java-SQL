81035f7ba4cf3470cc00a38f6cca167f977b32fa
Vinicius Guerra <102999190+Vinicius-Guerra@users.noreply.github.com>
2023-04-27 18:38:36 -0300

    att

--- byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -1,5 +1,8 @@
 package br.com.alura.bytebank;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -13,5 +16,15 @@ public class ConnectionFactory {
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
+    }
+    
+    private HikariDataSource createDataSource()  {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:mysql://localhost:3306/byte_bank");
+        config.setUsername("root");
+        config.setPassword("Vinicius.10");
+        config.setMaximumPoolSize(10);
+        
+        return new HikariDataSource(config);
     }
 }
