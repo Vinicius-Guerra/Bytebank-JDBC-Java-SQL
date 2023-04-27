6025d9cf6b609c29dee06ef68c5a8f035e5f8f3d
Vinicius Guerra <102999190+Vinicius-Guerra@users.noreply.github.com>
2023-04-27 15:52:39 -0300

    dependencias

--- byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
+++ byte-bank-alura-curso_jdbc_inicio/src/main/java/br/com/alura/bytebank/domain/conta/ContaService.java
@@ -70,10 +70,12 @@ public class ContaService {
     }
 
     private Conta buscarContaPorNumero(Integer numero) {
-        return contas
-                .stream()
-                .filter(c -> c.getNumero() == numero)
-                .findFirst()
-                .orElseThrow(() -> new RegraDeNegocioException("Não existe conta cadastrada com esse número!"));
+       Connection conn = connection.recuperarConexao();
+       Conta conta = new ContaDAO(conn).listarPorNumero(numero);
+       if(conta != null) {
+           return conta;
+       } else {
+           throw new RegraDeNegocioException("Nao existe conta cadastrada com esse numero!");
+       }
     }
 }
